4a3980ac74c7eeb666bd3a6ed0bb56b422131e2f
JuniorDCoder <foryoungjuniorngu@gmail.com>
2024-12-11 16:10:29 +0100

    feat: Installed AOS

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,9 +5,11 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
-
+AOS.init();
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) =>
@@ -19,6 +21,7 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
+            .use(AOS)
             .mount(el);
     },
     progress: {
